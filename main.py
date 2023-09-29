2670485be7c79dc278531daa6c68b8256e5dc9a1
ChristarM <146460668+ChristarM@users.noreply.github.com>
2023-09-29 18:25:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,14 @@ while True:
         print(m[parola.upper()])
         print("\n")
         time.sleep(1)
+    else:
+        print("Errore,Errore, autodistruzione tra:")
+        print("3")
+        time.sleep(0.5)
+        print("2")
+        time.sleep(0.5)
+        print("1")
+        time.sleep(0.5)
+        break
+while True:
+    print("ERRORE")
